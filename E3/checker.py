9779528e9aa24f3ac7c64eeecda04429b63d6b65
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2025-06-19 16:41:22 -0400

    Improve robustness of E3 and stmt checker
    - handle all Popen errors
    - properly reap E3 child process
    - fail on non-empty stderr or if stdout contains "error" before reading result file
    - print E3 stdout and stderr if non-empty
    - kill spawned checkers
    - improve Popen flags

--- E3/checker.py
+++ E3/checker.py
@@ -1,17 +1,21 @@
 import os
-import signal
 import json
 
-from E3.utils import *
-from subprocess import Popen, PIPE
+from E3.utils import (
+    ROOT_DIR,
+    format_lean_checker_file,
+    remove_error_source,
+    kill_process_group,
+)
+from subprocess import Popen, PIPE, SubprocessError, TimeoutExpired
 
 
 class Checker:
     def __init__(
         self,
-        nperms=3,
-        binTime=15,
-        approxTime=5,
+        n_perms=3,
+        bin_time=15,
+        approx_time=5,
         mode="skipApprox",
         tmp_path=os.path.join(ROOT_DIR, "tmp", "check"),
         result_path=os.path.join(ROOT_DIR, "results"),
@@ -21,44 +25,80 @@ class Checker:
         self.result_path = result_path
         os.makedirs(self.result_path, exist_ok=True)
 
-        self.nPermutations = nperms
-        self.equivSolverTime = binTime
-        self.approxSolverTime = approxTime
+        self.n_permutations = n_perms
+        self.equiv_solver_time = bin_time
+        self.approx_solver_time = approx_time
         self.mode = mode
 
-    def check(self, ground, test, instanceName):
-        tmpFile = os.path.join(self.tmp_path, instanceName + ".lean")
-        with open(tmpFile, "w") as file:
-            leanFile = format_lean_checker_file(ground, test)
-            file.write(leanFile)
-        outputJsonFile = os.path.join(self.result_path, instanceName + ".json")
+    def check(self, ground: str, test: str, instance_name: str) -> bool:
+        """
+        Check the logical equivalence of ``ground`` and ``test`` using E3, and write the
+        result to a file with the same name as the ``instance_name``.
+
+        :returns: ``True`` iff ``ground`` is equivalent to ``test``
+        """
+        tmp_file = os.path.join(self.tmp_path, instance_name + ".lean")
+        with open(tmp_file, "w") as file:
+            lean_file = format_lean_checker_file(ground, test)
+            file.write(lean_file)
+        output_json_file = os.path.join(self.result_path, instance_name + ".json")
+
         command = [
             "lake",
             "env",
             "lean",
             "--run",
-            tmpFile,
-            instanceName,
+            tmp_file,
+            instance_name,
             self.mode,
-            str(self.nPermutations),
-            str(self.equivSolverTime),
-            str(self.approxSolverTime),
+            str(self.n_permutations),
+            str(self.equiv_solver_time),
+            str(self.approx_solver_time),
             "true",
-            outputJsonFile,
+            output_json_file,
         ]
-        process = Popen(
-            command, stdin=PIPE, stdout=PIPE, cwd=ROOT_DIR, preexec_fn=os.setsid
-        )
-        try:
-            process.communicate()
-            with open(outputJsonFile, "r", encoding="utf-8") as f:
-                data = json.load(f)
 
-            result = data[instanceName]["binary_check"]
-            if result == "equiv":
-                return True
-            else:
-                return False
-        except:
-            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        process: Popen[str] | None = None
+        try:
+            with Popen(
+                command,
+                stdout=PIPE,
+                stderr=PIPE,
+                cwd=ROOT_DIR,
+                start_new_session=True,
+                text=True,
+                encoding="utf-8",
+            ) as process:
+                stdout, stderr = map(
+                    lambda x: remove_error_source(x.strip()), process.communicate()
+                )
+
+                if stdout:
+                    print(stdout)
+                if stderr:
+                    print(stderr)
+
+                if "error" in stdout or stderr:
+                    return False
+
+                try:
+                    with open(output_json_file, "r", encoding="utf-8") as f:
+                        data = json.load(f)
+                    return data[instance_name]["binary_check"] == "equiv"
+                except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
+                    print(f"⚠️  Failed to parse output file: {e}")
+                    return False
+        except (SubprocessError, OSError) as e:
+            print(f"⚠️  Failed to execute checker: {e}")
             return False
+        finally:
+            if process and process.pid:
+                kill_process_group(process.pid)
+
+                # allow up to 2 s to consume pipes and reap process
+                try:
+                    process.communicate(timeout=2)
+                except (ValueError, TimeoutExpired):
+                    # if ValueError: communicate() was already called, no problem
+                    # if TimeoutExpired: give up to avoid hanging the program
+                    pass
